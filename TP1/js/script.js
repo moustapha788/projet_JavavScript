0c58deb097dd19c093a33479586246cef4f9ccfc
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-02-18 14:26:50 +0000

    gestion des scripts : ajout de notes suppression de note édition de note

--- TP1/js/script.js
+++ TP1/js/script.js
@@ -7,9 +7,111 @@ function removeNote(idRemove) {
 
 
 
-// a function who create a note
-function create(idParent) {
-    const parent = getElementById(idParent);
+
+(function() {
+
+    // !AJOUT D'UN NOUVEL NOTE
+    const addNote = document.getElementById('addNote');
+    let i = 0;
+    addNote.addEventListener('click', () => {
+
+        i++;
+        // ! CRÉATION DES COMPOSANTS DE LA NOTE
+        // todo récupération du bag de notes
+        const noteContainer = document.getElementById('noteContainer');
+        // todo création du cadre de la note
+        const note = document.createElement('div');
+        // ! CRÉATION DU CADRE DU MENU
+        const noteMenu = document.createElement('div');
+        // * création de l'item éditer
+        const editNote = document.createElement('span');
+        const editNoteImage = document.createElement('i');
+        // * création de l'item éditer
+        const deleteNote = document.createElement('span');
+        const deleteNoteImage = document.createElement('i');
+        // ! THE NOTE IT SELF
+        const noteText = document.createElement('div');
+        const addText = document.createElement('textarea');
 
 
-}
+        // ! ORGANISATION DES ÉLÉMENTS
+        noteContainer.appendChild(note);
+        // * ajout du menu dans la note
+        note.appendChild(noteMenu);
+        // *ajout de noteText dans la note
+        note.appendChild(noteText);
+        // * ajout des icones dans les span
+        editNote.appendChild(editNoteImage);
+        deleteNote.appendChild(deleteNoteImage);
+        // * ajout des span dans le menu
+        noteMenu.appendChild(editNote);
+        noteMenu.appendChild(deleteNote);
+
+        // *ajout du textearrea dans  noteText
+        noteText.appendChild(addText);
+
+
+
+        // ! ajout de style à mes éléments
+        // *note
+        note.setAttribute('class', 'note');
+        // todo noteMenu
+        noteMenu.setAttribute('class', 'noteMenu');
+        // *editNote
+        editNote.setAttribute('class', 'editNote');
+        editNote.setAttribute('id', `editNote${i}`);
+        editNoteImage.setAttribute('class', 'fa-solid fa-pen-to-square');
+        // *deleteNote
+        deleteNote.setAttribute('class', 'deleteNote');
+        deleteNote.setAttribute('id', `deleteNote${i}`);
+        deleteNoteImage.setAttribute('class', 'fa-solid fa-trash-can');
+        // todo noteText
+        // *noteText
+        noteText.setAttribute('id', `noteText${i}`);
+        noteText.setAttribute('class', 'noteText');
+        // *addText
+        addText.setAttribute('id', `addText${i}`);
+        addText.setAttribute('class', 'addText');
+
+        // ! supprimer la note
+        deleteNote.addEventListener('click', () => {
+            noteContainer.removeChild(note);
+        });
+        // !editer la note
+        editNote.addEventListener('click', () => {
+            addText.toggleAttribute('disabled');
+            addText.focus();
+        });
+
+    }, false);
+
+
+
+
+
+
+})();
+
+
+/* 
+
+<!-- TODO ONE NOTE -->
+<div id="note" class="note">
+    <!-- !MENU OF THE NOTE -->
+    <div class="noteMenu">
+        <!-- * edit the note -->
+        <span class="editNote" id="editNote">
+            <i class="fa-solid fa-pen-to-square"></i>
+        </span>
+        <!-- * delete the note -->
+        <span class="deleteNote" id="deleteNote">
+            <i class="fa-solid fa-trash-can" ></i>
+        </span>
+    </div>
+    <!-- !THE NOTE IT SELF -->
+    <div class="noteText" id="noText">
+        <textarea class="addText" id="addText" cols="30" rows="10"></textarea>
+    </div>
+</div>
+<!-- fin de ma note -->
+*/
